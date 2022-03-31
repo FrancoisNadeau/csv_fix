bb87ba26ee5ba34977e77071cea64dc33962cd02
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2022-03-31 12:20:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ readme = Path(__file__).parent.joinpath('README.md').read_text()
 
 setup(
     name='csvfix',
-    version='0.0.1',
     packages=find_packages(),
     python_requires='>=3.8',
     url='https://github.com/FrancoisNadeau/csv_fix.git',
